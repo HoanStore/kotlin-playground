9268ec92388ccd989fb578a78d628d852bee2586
조근완 <kwjo@mqnic.com>
2025-06-27 17:21:18 +0900

    study: 스마트 캐스트 학습

---
+++ src/main/kotlin/com/hoan/kotlinplayground/basic/SmartCast.kt
@@ -0,0 +1,13 @@
+package com.hoan.kotlinplayground.basic
+
+fun main(args: Array<String>) {
+    printLength("Hello")  // 문자열 길이: 5
+
+}
+
+fun printLength(obj: Any) {
+    if (obj is String) {
+        println(obj.length) // obj가 String임을 검사했기 때문에 자동으로 String으로 취급
+    }
+}
+
